e19fefe6675872a040232f5df839cb068dd6e96c
unknown <negi60239@gmail.com>
2024-04-30 18:47:48 +0530

    task4

--- src/components/States.js
+++ src/components/States.js
@@ -1,109 +1,125 @@
-import React, { useEffect, useState } from 'react';
-import { Countries, CountryState, StateCity } from './serverApi';
-import './state.css';
+import React, { useEffect, useState } from 'react'
+import { Countries,CountryState ,StateCity} from './serverApi'
+import "./state.css"
 
 const States = () => {
-  const [countries, setCountry] = useState([]);
-  const [states, setStates] = useState([]);
-  const [cities, setCity] = useState([]);
-  const [selectedCountry, setSelectedCountry] = useState('');
-  const [selectedState, setSelectedState] = useState('');
-  const [selectedCity, setSelectedCity] = useState('');
-  const [countryError, setCountryError] = useState(null);
-  const [stateError, setStateError] = useState(null);
+    const [countries , setCountry] = useState([])
+    const [states , setStates] = useState([])
+    const [cities , setCity] = useState([])
+    const [selectedCountry , setSelectedCountry] = useState("")
+    const [ selectedState, setSelectedState] = useState("")
+    const [selectedCity , setSelectedCity] = useState("")
 
-  useEffect(() => {
-    const getCountry = async () => {
-      try {
-        const responseCountry = await Countries();
-        const countryData = responseCountry.data;
-        setCountry(countryData);
-      } catch (error) {
-        setCountryError('Error fetching countries');
-      }
-    };
 
-    getCountry();
-  }, []);
 
-  const handleCountryChange = async (e) => {
-    const countryName = e.target.value;
-    setSelectedCountry(countryName);
-    setStates([]);
-    setSelectedState('');
-    setSelectedCity('');
+    let getCountry = async()=>{
 
-    try {
-      const responseState = await CountryState(countryName);
-      const data = responseState.data;
-      setStates(data);
-    } catch (error) {
-      setStateError('Error fetching states');
+        let responseCountry = await Countries()
+        let countryData = responseCountry.data
+
+          setCountry(countryData);
     }
-  };
 
-  const handleStateChange = async (e) => {
-    const stateName = e.target.value;
-    setSelectedState(stateName);
-    setSelectedCity('');
 
-    try {
-      const responseCity = await StateCity(selectedCountry, stateName);
-      const data = responseCity.data;
-      setCity(data);
-    } catch (error) {
-      console.error('Error fetching cities:', error);
+    useEffect(()=>{
+                 
+       getCountry();
+
+    })
+
+
+
+    const handleCountryChange = async(e)=>{
+
+        let countryName = e.target.value
+         setSelectedCountry(countryName)
+            
+        let  responseState = await CountryState(countryName)
+        let  data = responseState.data
+               setStates(data)
+
+               setSelectedState("")
+               setSelectedCity("")
     }
-  };
 
-  const handleCityChange = (e) => {
-    const cityName = e.target.value;
-    setSelectedCity(cityName);
-  };
+    const handleStateChange = async(e)=>{
+         
+        let stateName = e.target.value
 
-  return (
+        setSelectedState(stateName)
+
+        let responseCity = await StateCity(selectedCountry,stateName);
+
+        let data = responseCity.data
+
+        setCity(data)
+
+        setSelectedCity("")
+
+    }
+
+    const handleCityChange = (e)=>{
+
+         let cityName = e.target.value
+
+         setSelectedCity(cityName)
+    }
+
+
+  
+    return (
     <div>
-      {countryError && <div>Error fetching countries: {countryError}</div>}
-      {stateError && <div>Error fetching states: {stateError}</div>}
-      <div className="selecte-country">
-        <h1>Select Location</h1>
-        <div className="selecte-country-name">
-          <div className="countries">
-            <select value={selectedCountry} onChange={handleCountryChange}>
-              <option value="">Select Country</option>
-              {countries.map((country) => (
-                <option key={country} value={country}>
-                  {country}
-                </option>
-              ))}
-            </select>
-          </div>
+                 
+          <div className="selecte-country">
+                  <h1>Select Location</h1>     
+                  <div className="selecte-country-name">
+             <div className="countries">
+                <select value = {selectedCountry} onChange={handleCountryChange}>
+                    <option value="">Select Country</option>
+                    {countries.map((country)=>(
+                          
+                          <option  key ={country}    value={country}>
+                                   
+                                   {country}
+                          </option>
+                        
+                    ))}
+                </select>
+             </div>
 
-          <div className="states">
-            <select value={selectedState} onChange={handleStateChange} disabled={!selectedCountry}>
-              <option value="">Select State</option>
-              {states.map((countryState) => (
-                <option key={countryState} value={countryState}>
-                  {countryState}
-                </option>
-              ))}
-            </select>
-          </div>
+             <div className="states">
+             <select value = {selectedState} onChange={handleStateChange} disabled={!selectedCountry}>
+                    <option value="">Select State</option>
+                    {states.map((countryState)=>(
+                          
+                          <option  key ={countryState}    value={countryState}>
+                                   
+                                   {countryState}
+                          </option>
+                        
+                    ))}
+                    </select>
+             </div>
 
-          <div className="cities">
-            <select value={selectedCity} onChange={handleCityChange} disabled={!selectedState}>
-              <option value="">Select Cities</option>
-              {cities.map((city) => (
-                <option key={city} value={city}>
-                  {city}
-                </option>
+
+             <div className="cities">
+             <select value={selectedCity} onChange={handleCityChange}  disabled ={!selectedState}>
+                <option value="">Select City</option>
+              {cities.map((city)=>(
+                   
+                   <option key={city} value={city}>
+                    {city}
+                   </option>
               ))}
-            </select>
-          </div>
-        </div>
-      </div>
+
+             </select>
+             
+             </div>
+            
+            </div> 
+</div>
     </div>
-  );
-};
+  )
+}
 
-export default States;
+export default States
