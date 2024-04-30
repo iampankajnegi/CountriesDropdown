794a088fd00fc5cd84b494c5a3d2f416fe10eaf2
unknown <negi60239@gmail.com>
2024-04-30 17:54:27 +0530

    task2

--- src/components/serverApi.js
+++ src/components/serverApi.js
@@ -5,22 +5,37 @@ import axios from "axios"
 
 export  const Countries = async()=>{
 
+    try{
+
        let response = await axios.get("https://crio-location-selector.onrender.com/countries")
 
        return response
+
+    }
+    catch(error) { console.error('Error fetching countries:', error)};
 }
 
 export const CountryState = async(countryName)=>{
 
+    try{
+
     let response = await axios.get(`https://crio-location-selector.onrender.com/country=${countryName}/states`)
 
     return response
+
+    }
+    catch(error) { console.error('Error fetching State:', error)};
 }
 
 
 export const StateCity = async(selectedCountry , stateName)=>{
 
+    try{
+
      let response = await axios.get(`https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${stateName}/cities`)
 
      return response
+
+    }
+    catch(error) { console.error('Error fetching Cities:', error)};
 }
